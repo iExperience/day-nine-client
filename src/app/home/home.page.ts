cbb5cdf9e81a22b00a172e792d36ff20b6f3b81d
Mikael von Ketelhodt <vonketelhodtm@gmail.com>
2019-06-06 09:37:21 +0100

    Client post

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  public user: any = {
+    name: "",
+    email: "",
+    password: ""
+  };
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
+  submit() {
+    console.log("Submitting to the server...");
+    console.log(this.user);
+
+    this.httpClient
+      .post("http://localhost:3000/api/users", this.user)
+      .subscribe(
+        (response) => {
+          console.log(response);
+        }
+      );
+  }
 }
